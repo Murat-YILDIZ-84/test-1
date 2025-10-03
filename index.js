7e1fe6ef4e1ac4440fe1765080d48bb1536fefcc
Coders Club <hi@codersclub.com>
2025-10-03 18:08:10 +0300

    commit-25

--- index.js
+++ index.js
@@ -22,12 +22,7 @@ app.get("/test", (req, res) => {
 app.get("/sunucu", (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/javascript'});
     //res.write(req.url.split("?")[1]);
-    res.write("<script>");
-    res.write("var url = 'https:\/\/livestream.ibb.gov.tr\/cam_turistik\/b_kapalicarsi.stream\/playlist.m3u8';");
-    res.write("var bradmaxPlayerConfig = { 'showErrorDetails':false, 'contextMenuDisabled': true, 'dataProvider':{'source':[{'url':url}]}, 'autoplay':true, 'mute':true };");
-    res.write("var element = document.getElementById('bradmaxPlayer');");
-    res.write("player = window.bradmax.player.create(element, bradmaxPlayerConfig);");
-    res.write("</script>");
+    res.write("https://livestream.ibb.gov.tr/cam_turistik/b_kapalicarsi.stream/playlist.m3u8");
     res.end();
 })
 
